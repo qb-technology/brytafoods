5cc01970439484903100758b34f649e865688fa2
Cybandy <andyboat75@gmail.com>
2026-03-05 20:14:49 +0100

    fix(index.vue): add missing closing bracket for useSeoMeta and format useHead

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,7 +9,15 @@ useSeoMeta({
   title,
   ogTitle: title,
   description,
-  ogDescription: description
+  ogDescription: description,
+})
+
+useHead({
+  meta: [
+    {
+      name: 'google-site-verification', content: 'Dh1_L9eObyBe9qvfY6DAR1h9N-K6l78D8nzSdnHehI'
+    }
+  ]
 })
 </script>
 
